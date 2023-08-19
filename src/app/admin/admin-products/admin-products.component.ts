c39a50da5744f451b68bcd99377038d39fe458e2
glennmargg <mendozaglennmar1@gmail.com>
2023-08-19 23:22:16 +0800

    gshoppe

---
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-products',
+  templateUrl: './admin-products.component.html',
+  styleUrls: ['./admin-products.component.css']
+})
+export class AdminProductsComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,56 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ErrorComponent } from './error/error.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MyAccountComponent } from './my-account/my-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent,
+    CheckoutComponent,
+    ErrorComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent,
+    MyAccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'cart', component: CartComponent },
+      { path: 'checkout', component: CheckoutComponent },
+      { path: 'error', component: ErrorComponent },
+      { path: 'login', component: LoginComponent},
+      { path: 'my-orders', component: MyOrdersComponent },
+      { path: 'my-account', component: MyAccountComponent },
+      { path: 'navbar', component: NavbarComponent },
+      { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'products', component: ProductsComponent},
+      { path: 'admin/orders', component: AdminOrdersComponent},
+      { path: 'admin/products', component: AdminProductsComponent}
+      
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
